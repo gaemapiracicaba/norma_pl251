8c12cce37b527a50a4fc39be8a847d300b7a5af0
Michel Metran <michelmetran@gmail.com>
2021-12-10 12:53:10 -0300

    Ajustes

--- test/paths.py
+++ test/paths.py
@@ -1,12 +1,16 @@
 import os
 
-input_path = os.path.join('..', 'data')
-tabs_path = os.path.join(input_path, 'tabs')
-maps_path = os.path.join(input_path, 'maps')
-shps_path = os.path.join(input_path, 'shps')
-imgs_path = os.path.join(input_path, 'imgs')
-
+input_path = os.path.join('..', 'data', 'input')
+output_path = os.path.join('..', 'data', 'output')
 os.makedirs(input_path, exist_ok=True)
+os.makedirs(output_path, exist_ok=True)
+
+tabs_path = os.path.join(output_path, 'tabs')
+maps_path = os.path.join(output_path, 'maps')
+shps_path = os.path.join(output_path, 'shps')
+imgs_path = os.path.join(output_path, 'imgs')
+
+
 os.makedirs(tabs_path, exist_ok=True)
 os.makedirs(maps_path, exist_ok=True)
 os.makedirs(shps_path, exist_ok=True)
